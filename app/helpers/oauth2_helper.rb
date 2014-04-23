03358463f709a5c6e078f77f32e463cb6700151c
CharleZhu <zhuchuantao1990@126.com>
2014-04-23 16:03:03 +0800

    modify

--- app/helpers/oauth2_helper.rb
+++ app/helpers/oauth2_helper.rb
@@ -6,7 +6,7 @@ module Oauth2Helper
   #请求openId
   REQUEST_OPENID_URL="https://graph.qq.com/oauth2.0/me"
   #请求详参
-  APPID="223448"
+  APPID="101069759"
   REQUEST_ACCESS_TOKEN={
     :response_type=>"token",
     :client_id=>APPID,
@@ -14,5 +14,4 @@ module Oauth2Helper
     :scope=>"get_user_info,add_topic,add_pic_t,add_share,add_t",
     :state=>"1"
   }
-
 end
