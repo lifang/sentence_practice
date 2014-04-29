0746e1aa47d061242845c4d6d433fdf54f85dd0c
CharleZhu <zhuchuantao1990@126.com>
2014-04-29 17:06:29 +0800

    modify

--- app/models/question.rb
+++ app/models/question.rb
@@ -7,20 +7,24 @@ class Question < ActiveRecord::Base
 	LEVEL_NAME = {0 => "入门", 1 => "初级", 2 => "中考", 3 =>"高考", 4 =>"四级", 5 => "六级", 6 => "出国"}     
 
 	def self.get_next_question user
-		questions_count = Question.where(["level_types = ?", user.level]).count
-		answer_details = AnswerDetail.where(["first_status = ? and user_id = ?", true, user.id])
-		step = "first"
-		if answer_details.length == questions_count
-			step = "second"
-			answer_details = AnswerDetail.where(["second_status = ? and user_id = ?", true, user.id])
-		end
-		answer_details_id = answer_details.map(&:question_id)
-		if answer_details_id.any?
-			question = Question.where(["level_types = ? and id not in (?)",user.level, answer_details_id]).order("Rand()").limit(1)
-		else
-			question = Question.where(["level_types = ?", user.level]).order("Rand()").limit(1)
+		questions = Question.where(["level_types = ?", user.level])
+		if questions.any?
+			questions_id = questions.map(&:id)
+			questions_count = questions.length if questions.any?
+			answer_details = AnswerDetail.where(["first_status = ? and user_id = ? and question_id in (?)", true, user.id, questions_id])
+			step = "first"
+			if answer_details.length == questions_count
+				step = "second"
+				answer_details = AnswerDetail.where(["second_status = ? and user_id = ? and question_id in (?)", true, user.id, questions_id])
+			end
+			answer_details_id = answer_details.map(&:question_id)
+			if answer_details_id.any?
+				question = Question.where(["level_types = ? and id not in (?)",user.level, answer_details_id]).order("Rand()").limit(1)
+			else
+				question = Question.where(["level_types = ?", user.level]).order("Rand()").limit(1)
+			end	
+			question = question.first
 		end	
-		question = question.first
 		{:question => question, :step => step}
 	end         
 end
