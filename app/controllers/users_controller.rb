a2ff03bfaffbdde7b8d7805a0e29bd9d1bb844c5
CharleZhu <zhuchuantao1990@126.com>
2014-04-29 16:29:03 +0800

    modify

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,12 +55,21 @@ class UsersController < ApplicationController
 		user_id = cookies[:user_id]
 		@correct_counts = url.scan(/correct_counts\=.*/).first.to_s.gsub(/correct_counts\=/,"").to_i
 		user = User.find_by_id user_id
+		info = Question.get_next_question user
+		@question = info[:question]
+		@step = info[:step]	
 		current_level_questions = Question.where(["level_types = ?",user.level])
 		current_level_questions_id = current_level_questions.map(&:id)
 		@current_level_questions_count = current_level_questions.length
-		@finish_questions_count = AnswerDetail.where(["question_id in (?) and second_status = ? and user_id = ?",
+		if @step == "first"
+			@finish_questions_count = AnswerDetail.where(["question_id in (?) and first_status = ? and user_id = ?",
 				current_level_questions_id, true, user.id]).count if current_level_questions_id.present? && 
 								current_level_questions_id.any?
+		else
+			@finish_questions_count = AnswerDetail.where(["question_id in (?) and second_status = ? and user_id = ?",
+				current_level_questions_id, true, user.id]).count if current_level_questions_id.present? && 
+								current_level_questions_id.any?
+		end	
 	end	
 
 	#解锁等级
