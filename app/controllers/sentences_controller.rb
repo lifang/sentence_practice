e527f5555c14b7ab1d680651be5541e805939748
amanda <mengfanling825@163.com>
2014-04-30 14:13:44 +0800

    update open_id

--- app/controllers/sentences_controller.rb
+++ app/controllers/sentences_controller.rb
@@ -7,6 +7,7 @@ class SentencesController < ApplicationController
 		if user_id.present?
 			@user = User.find_by_id user_id.to_i
 			if @user.present?
+        @user.update_attribute(:open_id, params[:open_id]) if params[:open_id]
 				redirect_to :controller => :users, :action => :index	
 			end	
 		end	
